4a7bff6d871390695ad8d01a23654eb4918c3a47
ddthomas <dennison.david.thomas@gmail.com>
2019-12-31 16:57:06 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { StyleSheet, Text, View } from "react-native";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={styles.root}>
+      <Text>CookWithMeTV</Text>
+      <Text>v1</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+ root: {
+   justifyContent: 'center',
+   alignItems: 'center'
+ }
+});
 
 export default App;
