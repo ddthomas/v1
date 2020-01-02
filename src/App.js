40e90138f184aeb517f556333249bcbf028e1f48
ddthomas <dennison.david.thomas@gmail.com>
2020-01-01 18:17:06 -0600

    none

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 
 import './App.css';
 
+const Mux = require('@mux/mux-node');
+const { Video, Data } = new Mux("4afee5d3-8384-4a09-b67a-365e9a41caf8", "Yl+lU10Wg+JPSWmEYsPUp9ATBt0SXmIxRzKHXfeIf1kZd9auO+SObJlKrFkBM6ub3XK8dZQVTJc");
+
 function App() {
   return (
     <View style={styles.root}>
